9b65508d643113acdb954854ac6b3d9cbca1874d
a-s-t-e-y-a <krishnoit100@gmail.com>
2025-11-04 13:26:21 +0530

    FEAT: change url again due vm failure

--- frontend/lib/axios.js
+++ frontend/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://210.79.129.22:4000/api',
+  baseURL: 'http://210.79.128.220:4000/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
